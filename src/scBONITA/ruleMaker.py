9b5477e360ffba397fc4bb6d9f3c6d834ef2846f
Mukta G. Palshikar <32854513+mgp13@users.noreply.github.com>
2022-04-14 12:43:26 -0400

    Add files via upload

--- src/scBONITA/ruleMaker.py
+++ src/scBONITA/ruleMaker.py
@@ -353,8 +353,10 @@ class ruleMaker:
                 tempandNodeInvertList.append([0, 0, 0])
             tempandnoder.append(tempAndNodes)
             tempandinverter.append(tempandNodeInvertList)
-        self.andNodeInvert = np.array(tempandinverter, dtype=np.intc, order="C")
-        self.andNodes = np.array(tempandnoder, dtype=np.intc, order="C")
+        #self.andNodeInvert = np.array(tempandinverter, dtype=np.intc, order="C")
+        self.andNodeInvert = np.array(tempandinverter, dtype=object, order="C")
+        #self.andNodes = np.array(tempandnoder, dtype=np.intc, order="C")
+        self.andNodes = np.array(tempandnoder, dtype=object, order="C")
 
     def __genRandBits(self):
         """generates a random bitstring"""
@@ -1096,7 +1098,7 @@ class ruleMaker:
         indLen = len(nodeIndividual)
         andNodes = np.array(model.andNodes, dtype=np.intc, order="C")
         nodeNum = len(model.nodeList)
-        andNodeInvert = np.array(model.andNodeInvert, dtype=np.intc, order="C")
+        andNodeInvert = np.array(model.andNodeInvert, dtype=object, order="C")
         individualParse = np.array(model.individualParse, dtype=np.intc, order="C")
         andLenList = np.array(model.andLenList, dtype=np.intc, order="C")
         nodePositions1 = model.nodePositions
