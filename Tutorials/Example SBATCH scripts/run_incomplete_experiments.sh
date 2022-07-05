653895c9599e6b921b04e86332bdcb0673079b63
Mukta G. Palshikar <32854513+mgp13@users.noreply.github.com>
2022-07-05 15:33:08 -0400

    Intelligently select jobs to re-execute based on completion status

--- Tutorials/Example SBATCH scripts/run_incomplete_experiments.sh
+++ Tutorials/Example SBATCH scripts/run_incomplete_experiments.sh
@@ -11,23 +11,27 @@
 for d in */ ; do
     echo "$d"
     cd $d
-    rm *.graphml
-    rm *.log
-    rm *.so 
+    #rm *.graphml
+    #rm *.log
+    #rm *.so 
     cd ..
-    cp *.sh $d
-    cp *.py $d
-    cp makefile $d
-    cp *.graphml $d
-    cp *c $d
+    #cp *.sh $d
+    #cp *.py $d
+    #cp makefile $d
+    #cp *.graphml $d
+    #cp *c $d
     cd $d
     for item in *_scoreNodes.sh
     do
         base="${item%??????????????}"
         out="$base""_importanceScores.csv"
         input="$base""_scoreNodes.sh"
-        [ -f "$input" -a ! -f "$out" ] && echo "$input is missing $out" >&2
-        sbatch $input
+        localSearch="$base""_localErrors1.pickle"
+        globalSearch="$base""_out2.pickle"
+        #[ -f "$input" -a ! -f "$out" ] && echo "$input is missing $out" >&2 #missing importance score calculations
+        #[ -f "$input" -a ! -f "$localSearch" ] && echo "$input is missing $localSearch" >&2 #missing local search output
+        [ -f "$input" -a ! -f "$globalSearch" ] && echo "$input is missing $globalSearch" >&2 #missing global search output
+        [ -f "$input" -a ! -f "$globalSearch" ] && sbatch $input #(re)execute jobs where global search has not completed
     done
 cd ..
 done
