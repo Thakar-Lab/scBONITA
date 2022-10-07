db43bdff2b3e8e88d2634495865aebebf1dbea0d
Mukta G. Palshikar <32854513+mgp13@users.noreply.github.com>
2022-10-07 17:30:59 -0400

    setup script for Rachel

--- Tutorials/Example SBATCH scripts/scBonita_setup.sh
+++ Tutorials/Example SBATCH scripts/scBonita_setup.sh
@@ -7,7 +7,7 @@
 #SBATCH -c 1
 #SBATCH --mem=60G
 
-module load anaconda3/2020.07
+module load anaconda3/2020.07 #MODIFY THIS
 
 #compile required C code
 make
@@ -17,8 +17,8 @@ make
 source activate scBonita
 
 #Initiate run
-
-python3 pipeline.py --fullPipeline 1 --maxNodes 20000 --separator "," --getKEGGPathways True --organism "hsa" --generateSbatch True --partition standard --time 48:00:00 --binarizeThreshold 0.1 --memory 60G
+# MODIFY condaEnv
+python3 pipeline.py --dataFile "Kazer2020expr.csv" --fullPipeline 1 --maxNodes 20000 --separator "," --getKEGGPathways True --organism "hsa" --generateSbatch True --condaEnv "scBonita2" --pythonVersion "python3" --sampleCells True --parallelSearch True --memory "60G" --partition "standard" --binarizeThreshold 0.1
 
 #Check the progress of jobs using:
 #squeue -u yourNETID
